6d3befcc1b1da1709973c83042995fe05190955c
Tim <a.reflesh@gmail.com>
2021-05-20 12:17:27 -0400

    connection fixing

--- main.py
+++ main.py
@@ -6,27 +6,38 @@ import SessionState
 from plyer import notification
 import pysftp
 import paramiko
+from paramiko import RSAKey
+from base64 import decodebytes
 st.set_page_config(layout="wide")
 state = SessionState.get(n = 0, file_list=os.listdir("./paintings/images/"))
 @st.cache
-def dir_creation(): 
-    with pysftp.Connection('mt1.bsc.es', username='bsc21438', password='02t8q1uV') as sftp:
-        if not sftp.exists("/gpfs/home/bsc21/bsc21438/paintings/"+name+"/"):
-            sftp.makedirs("/gpfs/home/bsc21/bsc21438/paintings/"+name+"/")
+def create_folder(user_name):
+    keydata = b"""AAAAB3NzaC1yc2EAAAABIwAAAQEAvZYcvomQbQ72DDZtV3EHXCLHzlj4oSHwtp+9qhm3H3xSWxm0mVX3xgpLkljCtWycZYtKr8G8NqK97FL42r40W++LqjRkRpOzJ8SzkV+gh365FvouGhRSChMsnuCwyiAQnhCT1wfdadxh7/6+fm3RNFjoR3MMXsjabAztD+ZXcVJzTZ1Q4yRPV79Q2wa4zp3syrda1g1+3cLMlPwVGTTaPrhZwNnDYOM+C8PDr4cr88bHNkcAp2keINV9yZUNJzJNSrWg8g9sU9/e+ffd61n9jQDZlD2hasffkCeos92HNwkVgXws5xPo+kbuisFEIcybKu5Yrbb+U8RK9JCbd/2//w=="""
+    key = paramiko.RSAKey(data=decodebytes(keydata))
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys.add('mt1.bsc.es', 'ssh-rsa', key) 
+    with pysftp.Connection('mt1.bsc.es', username='bsc21438', password='02t8q1uV', cnopts=cnopts) as sftp:
+        if not sftp.exists("/gpfs/home/bsc21/bsc21438/paintings/"+user_name+"/"):
+            sftp.makedirs("/gpfs/home/bsc21/bsc21438/paintings/"+user_name+"/")
     sftp.close()
+
+@st.cache
+def upload_file(original_file, dest_file):
+    keydata = b"""AAAAB3NzaC1yc2EAAAABIwAAAQEAvZYcvomQbQ72DDZtV3EHXCLHzlj4oSHwtp+9qhm3H3xSWxm0mVX3xgpLkljCtWycZYtKr8G8NqK97FL42r40W++LqjRkRpOzJ8SzkV+gh365FvouGhRSChMsnuCwyiAQnhCT1wfdadxh7/6+fm3RNFjoR3MMXsjabAztD+ZXcVJzTZ1Q4yRPV79Q2wa4zp3syrda1g1+3cLMlPwVGTTaPrhZwNnDYOM+C8PDr4cr88bHNkcAp2keINV9yZUNJzJNSrWg8g9sU9/e+ffd61n9jQDZlD2hasffkCeos92HNwkVgXws5xPo+kbuisFEIcybKu5Yrbb+U8RK9JCbd/2//w=="""
+    key = paramiko.RSAKey(data=decodebytes(keydata))
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys.add('mt1.bsc.es', 'ssh-rsa', key) 
+    with pysftp.Connection('mt1.bsc.es', username='bsc21438', password='02t8q1uV', cnopts=cnopts) as sftp:
+        sftp.put(original_file, dest_file)
+    sftp.close()
+
 name = st.sidebar.text_input("Input your name and press Enter please:","")
 if (name!=''):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('mt1.bsc.es', username='bsc21438', password='02t8q1uV')
-    file_sftp = ssh.open_sftp()
-
     work_dir = "./paintings/"+name+"/"
     if not os.path.exists(work_dir):
         os.mkdir(work_dir)
 
-    dir_creation()    
-    
+    create_folder(name)   
     try:
         image_name = state.file_list[state.n]
     except: 
@@ -69,11 +80,11 @@ if (name!=''):
 
         with open(work_dir+os.path.splitext(image_name)[0]+'.json', 'w') as json_file:
             json.dump(annot, json_file)
-        with file_sftp.open("/gpfs/home/bsc21/bsc21438/paintings/"+name+"/"+os.path.splitext(image_name)[0]+'.json', 'w') as outfile:
-            json.dump(annot, outfile)
+        upload_file(work_dir+os.path.splitext(image_name)[0]+'.json',"/gpfs/home/bsc21/bsc21438/paintings/"+name+"/"+os.path.splitext(image_name)[0]+'.json')
+        
     if col2.button("Next image",key = state.n):
         state.n=state.n+1
     if col2.button("Previous image",key = state.n):
         state.n=state.n-1
-    ssh.close()
+
 
