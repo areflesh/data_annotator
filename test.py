c063fcacfb81dc5edd241093259ae87db22d1fbb
Tim <a.reflesh@gmail.com>
2021-06-10 09:53:21 -0400

    pic udate

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from nltk.translate.bleu_score import sentence_bleu
 reference = [['the', 'cat',"is","sitting","on","the","mat"]]
 test = ["there",'is',"cat","sitting","cat"]
-score = sentence_bleu(  reference, test)
+score = sentence_bleu(reference, test)
 print(score)
